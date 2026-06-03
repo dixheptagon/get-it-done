57ead4f14d245b0bf69b51c765b985bbcc287eed
dixheptagon <forentino006@gmail.com>
2026-06-03 15:55:59 +0700

    feat(ui/mobile-infobar): add carousel component and utility functions
    Introduce carousel functionality to the weekly calendar using embla-carousel-react. This change also includes the addition of core UI utility functions, a new UI component directory, and Prettier configuration for consistent code formatting.

--- components/infobar/weeklyCalendar.tsx
+++ components/infobar/weeklyCalendar.tsx
@@ -3,41 +3,46 @@
 import { DayItem, getWeeklyDates } from "@/lib/date";
 import { setSelectedDate, useTodoStore } from "@/store/useTodoStore";
 import clsx from "clsx";
+import { Carousel, CarouselContent } from "../ui/carousel";
 
 export function WeeklyCalendar() {
   const selectedDate = useTodoStore((state) => state.selectedDate);
   const weeklyDates = getWeeklyDates();
 
   return (
-    <div className="flex justify-between items-center gap-3 overflow-hidden -mr-4 ">
-      {weeklyDates.map((day: DayItem) => {
-        const isActive = day.fullDate === selectedDate;
-        const isPast = new Date(day.fullDate) < new Date();
+    <Carousel>
+      <CarouselContent className="flex items-center justify-between gap-3">
+        {weeklyDates.map((day: DayItem) => {
+          const isActive = day.fullDate === selectedDate;
+          const isPast = new Date(day.fullDate) < new Date();
 
-        return (
-          <button
-            key={day.fullDate}
-            onClick={() => setSelectedDate(day.fullDate)}
-            className={clsx(
-              "flex flex-col items-center justify-center min-w-13 text-primar-800 flex-1 py-2 px-3 rounded-md focus:outline-none transition-color duration-300 ",
-              isActive
-                ? "bg-primary-800 text-primary-0 shadow-md font-bold"
-                : "bg-transparent  hover:text-primary-800/80",
-              isPast && !isActive && "text-primary-800/50",
-            )}
-          >
-            <span
-              className={"text-xs tracking-widest uppercase font-semibold mb-1"}
+          return (
+            <button
+              key={day.fullDate}
+              onClick={() => setSelectedDate(day.fullDate)}
+              className={clsx(
+                "text-primar-800 transition-color flex min-w-13 flex-1 flex-col items-center justify-center rounded-md px-3 py-2 duration-300 focus:outline-none",
+                isActive
+                  ? "bg-primary-800 text-primary-0 font-bold shadow-md"
+                  : "hover:text-primary-800/80 bg-transparent",
+                isPast && !isActive && "text-primary-800/50",
+              )}
             >
-              {day.dayName}
-            </span>
+              <span
+                className={
+                  "mb-1 text-xs font-semibold tracking-widest uppercase"
+                }
+              >
+                {day.dayName}
+              </span>
 
-            <span className="text-2xl font-bold tracking-tight">
-              {day.dayNum}
-            </span>
-          </button>
-        );
-      })}
-    </div>
+              <span className="text-2xl font-bold tracking-tight">
+                {day.dayNum}
+              </span>
+            </button>
+          );
+        })}
+      </CarouselContent>
+    </Carousel>
   );
 }
